ae43cf2e89209047dcd4a6fb2bceeaeeb3857b0f
nurbep <nuruzzaman.bepari123@gmail.com>
2025-11-14 23:14:14 -0600

    initial com mit

---
+++ src/test/java/talentai/com/eurekasever/talentai_eurekaserver/TalentaiEurekaserverApplicationTests.java
@@ -0,0 +1,13 @@
+package talentai.com.eurekasever.talentai_eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TalentaiEurekaserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
